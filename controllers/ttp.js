61eb56c32cccdbf65a4c627dd9a5ac94e1d7c773
Wanz-Botz <96960904+Wanz-Botz@users.noreply.github.com>
2022-01-17 07:39:33 +0700

    Add files via upload

---
+++ controllers/ttp.js
@@ -0,0 +1,27 @@
+const { cekKey, limitAdd, isLimit } = require('../database/db');
+const fetch = require('node-fetch')
+
+
+async function ttp(req, res) {
+    const { text, apikey } = req.query
+    if (!text) return res.status(200).send({
+        status: false,
+        message: `masukan parameter 'text'`
+    })
+    let message = await cekKey(apikey, res)
+    if (message) return res.status(200).send({
+        status: false,
+        message
+    })
+    try {
+        limitAdd(apikey)
+        buffer = await fetch(`https://hardianto.xyz/api/maker/ttp?text=${encodeURIComponent(text)}&apikey=hardianto`).then(v => v.buffer())
+        res.type('webp')
+        res.send(buffer)
+    } catch {
+        res.status(500).send({ status: false, message: 'Internal Server Error' })
+    }
+}
+
+
+module.exports = { ttp }
