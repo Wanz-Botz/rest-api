fbae7f9abba9ac817e1af651478f6eba4ceb15d7
Wanz-Botz <96960904+Wanz-Botz@users.noreply.github.com>
2022-01-25 06:16:36 +0700

    Add files via upload

---
+++ routes/api.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const { readFileTxt, readFileJson } = require('../lib/function');
+const { mp4, Mp3 } = require('../lib/youtube');
+const { cekKey, checkLimit, resetLimit } = require('../database/db'); 
+const { youtubePlay, youtubeMp4, youtubeMp3 } = require('../controllers/yt');
+const { cakLontong, bijak, quotes, fakta, ptl, motivasi } = require('../controllers/randomtext');
+const { photoOxy } = require('./oxy');
+const { tgContr } = require('../controllers/tebakgambar');
+const { ttp } = require('../controllers/ttp');
+const { attp } = require('../controllers/ttp');
+const { mDo } = require('../controllers/media');
+const { tIk } = require('../controllers/tik');
+const { tIk2 } = require('../controllers/tik2');
+
+router.get('/checkkey', async (req, res) => {
+    const apikey = req.query.apikey;
+   if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    const limit = await checkLimit(apikey);
+    res.send({status: 200, apikey: apikey, limit: limit});
+});
+
+router.get('/tiktok', tIk);
+
+router.get('/tiktok2', tIk2);
+
+router.get('/mediafire', mDo);
+
+router.get('/tebakgambar', tgContr);
+
+router.get('/ttp', ttp);
+
+router.get('/attp', attp);
+
+router.get('/ytplay', youtubePlay);
+
+router.get('/ytmp4', youtubeMp4);
+
+router.get('/ytmp3', youtubeMp3);
+
+router.get('/caklontong', cakLontong);
+
+router.get('/quotes', quotes);
+
+router.get('/fakta', fakta);
+
+router.get('/bijak', bijak);
+
+router.get('/ptl', ptl);
+
+router.get('/motivasi', motivasi);
+
+router.get('/oxy/:tema', photoOxy);
+
+module.exports = router;
