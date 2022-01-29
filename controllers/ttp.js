693f0b2aae99dbef50d2916dd26e5e29ddfcd91f
Wanz-Botz <96960904+Wanz-Botz@users.noreply.github.com>
2022-01-30 05:39:28 +0700

    Update ttp.js

--- controllers/ttp.js
+++ controllers/ttp.js
@@ -1,7 +1,5 @@
 const { cekKey, limitAdd, isLimit } = require('../database/db');
 const fetch = require('node-fetch')
-const base64 = require('base-64')
-
 
 async function ttp(req, res) {
     const { text, apikey } = req.query
@@ -16,11 +14,9 @@ async function ttp(req, res) {
     })
     try {
         limitAdd(apikey)
-        data = await fetch(`https://hardianto.xyz/api/maker/ttp?text=${encodeURIComponent(text)}&apikey=hardianto`).then(v => v.json())
-         base64 = data.base64
-         var buffer = base64.slice(22)
-         await fs.writeFileSync(`ttp.png`, buffer, 'base64')
-        res.sendFile(__path+'/ttp.png')
+        buffer = await fetch(`https://hardianto.xyz/api/maker/ttp?text=${encodeURIComponent(text)}&apikey=hardianto`).then(v => v.buffer())
+        res.type('webp')
+        res.send(buffer)
     } catch {
         res.status(500).send({ status: false, message: 'Internal Server Error' })
     }
